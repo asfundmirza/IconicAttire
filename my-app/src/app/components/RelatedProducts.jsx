863aec65cad75f04519e046763535abf209034e7
asfund <asfundmirzaj@gmail.com>
2023-10-26 01:26:20 +0500

    added homepage changes and design

--- my-app/src/app/components/RelatedProducts.jsx
+++ my-app/src/app/components/RelatedProducts.jsx
@@ -4,7 +4,13 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import ProductCard from "./ProductCard";
 
-const RelatedProducts = ({ products }) => {
+const RelatedProducts = ({
+  products,
+  ParentClassName,
+  innerClassName,
+  text,
+  innerParentClassName,
+}) => {
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -21,12 +27,20 @@ const RelatedProducts = ({ products }) => {
   };
 
   return (
-    <div className="mt-[50px] md:mt-[100px] mb-[100px] md:mb-0">
-      <div className="text-2xl font-bold mb-5">You Might Also Like</div>
+    <div
+      className={`mt-[50px] md:mt-[100px] mb-[100px] md:mb-0  ${
+        ParentClassName || ""
+      }`}
+    >
+      <div className={`flex w-full ${innerParentClassName || ""}`}>
+        <div className={`text-2xl font-bold mb-5 ${innerClassName || ""}`}>
+          {text ? text : "You Might Also Like"}
+        </div>
+      </div>
       <Carousel
         responsive={responsive}
         containerClass="-mx-[10px]"
-        itemClass="px-[10px]"
+        itemClass="px-[10px] "
       >
         {products?.data?.map((product) => (
           <ProductCard key={product?.id} data={product} />
